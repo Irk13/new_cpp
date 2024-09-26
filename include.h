a66652a75d007124c76befc7cf777bc06af2a822
Roman Ivanov2 <you@example.com>
2024-09-26 12:55:26 +0700

    Revert "test4"
    This reverts commit 341d287e86f5368dc336f7ceff16f2ea0ca90ede.

--- include.h
+++
@@ -1,4 +0,0 @@
-struct Kernel {
-    int a;
-    int b;
-};
--- main.cpp
+++ main.cpp
@@ -12,9 +12,7 @@ int main() {
     std::cout << "Roma Ivanov" << std::endl;
     std::cout << "Olya Ivanova" << std::endl;
     std::cout << "Lera Ivanova" << std::endl;
-    std::cout << "Kirill Ivanov" << std::endl;
-    std::cout << test_func2(a, b) << std::endl;
-    // lalala
+
     std::cout << flagi.b  << std::endl;
     std::cout << flagi.c  << std::endl;
     std::cout << flagi.d  << std::endl;
